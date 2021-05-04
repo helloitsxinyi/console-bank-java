82a15a334ad981556e9bcce700f3a19215a9e05a
Xinyi Tay <xinzwerkz@gmail.com>
2021-05-04 23:24:16 +0800

    Added first test

--- src/test/java/com/banking/BankAccountTest.java
+++ src/test/java/com/banking/BankAccountTest.java
@@ -1,7 +1,16 @@
 package com.banking;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class BankAccountTest {
 
+    BankAccount account = new BankAccount();
+
+    @Test
+    void shouldGetZero_givenNewAccount() {
+        assertThat(account).isEqualTo(0);
+    }
 }
+
