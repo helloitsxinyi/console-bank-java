fefe2c0bb536bb914477300f13bdbd89a8a77215
Xinyi Tay <xinzwerkz@gmail.com>
2021-05-05 11:25:47 +0800

    Implementation of basic account details, first test passed, included setup test

--- src/test/java/com/banking/BankAccountTest.java
+++ src/test/java/com/banking/BankAccountTest.java
@@ -1,22 +1,30 @@
 package com.banking;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class BankAccountTest {
 
+    BankAccount account;
+
+    @BeforeEach
+    void setUp() {
+        // given
+        account = new BankAccount();
+    }
+
     @Test
     void shouldGetCustomerNameAndId_whenNewAccount() {
 
-        // given
-        BankAccount account = new BankAccount();
-
         // when
         String custName = account.getCustomerName();
+        String custId = account.getCustomerId();
 
         // then
-        assertThat(custName).isEqualTo("");
+        assertThat(custName).isEqualTo("Xiao Ming");
+        assertThat(custId).isEqualTo("123");
     }
 
 
